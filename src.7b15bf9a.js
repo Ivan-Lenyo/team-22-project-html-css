e2e6ac8c89cd6d9094770c72b94b0769cfa279e9
Ivan-Lenyo <78181224+Ivan-Lenyo@users.noreply.github.com>
2021-05-16 22:22:36 +0000

    Deploying to gh-pages from @ Ivan-Lenyo/team-22-project-html-css@6eec6561c8840f045c0a6bf8d96fec85cff43e57 🚀

--- src.7b15bf9a.js
+++ src.7b15bf9a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-22-project-html-css/src.835ac0de.js.map
+//# sourceMappingURL=/team-22-project-html-css/src.7b15bf9a.js.map
